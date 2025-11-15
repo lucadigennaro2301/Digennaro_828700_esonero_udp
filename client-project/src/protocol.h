9337557a16616efdde6b7887cb0c0c57dfe859ce
Fabio Calefato <fabio.calefato@uniba.it>
2025-11-15 15:34:42 +0100

    Include stdint.h in protocol.h
    Added include for stdint.h to protocol.h

--- client-project/src/protocol.h
+++ client-project/src/protocol.h
@@ -8,6 +8,8 @@
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
+#include <stdint.h>
+
 // Shared application parameters
 #define SERVER_PORT 27015  // Server port (change if needed)
 #define BUFFER_SIZE 512    // Buffer size for messages
